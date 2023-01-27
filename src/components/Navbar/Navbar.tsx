87cbe7d8e98b37ba1624a0e40f8db5f1a7889438
Fred Lefebvre <fredlefevre@outlook.com>
2023-01-27 17:40:39 -0500

    Implemented login, logout and reset password functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,14 +2,17 @@ import { Flex, Image } from "@chakra-ui/react";
 import SearchInput from "./SearchInput";
 import React from "react";
 import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 const Navbar: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
       <Image src="/images/redditlogo.png" />
       {/* Directory */}
       <SearchInput />
-      <RightContent />
+      <RightContent user={user} />
     </Flex>
   );
 };
